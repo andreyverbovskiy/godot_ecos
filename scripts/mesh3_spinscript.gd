d6263bf6fbeaa62451bcd997548f61429070a814
Chyrion <laine.vili@gmail.com>
2024-02-21 13:24:57 +0200

    heh

---
+++ scripts/mesh3_spinscript.gd
@@ -0,0 +1,11 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta): 
+	rotate_y(0.1)
