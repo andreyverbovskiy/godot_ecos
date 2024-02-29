c4af0a9313a44f0cdf80277d8646d267eb27acc7
Chyrion <laine.vili@gmail.com>
2024-03-01 00:29:24 +0200

    Expanded slightly on custom Resource usage

--- scripts/Creature/creature.gd
+++ scripts/Creature/creature.gd
@@ -4,18 +4,38 @@ class_name Creature
 
 ## CreatureResource, gets populated as the Node3D object is initialized into the scene
 @export var stats: Resource
+@export var energy: int
+@export var food: int
+@export var water: int
+@export var age: int
+@export var attack: int
 
 func _init():
-    stats = load("res://resources/CreatureResource.tres")
-    stats.emit_changed()
+    if !stats:
+        stats = load("res://resources/CreatureResource.tres")
+    energy = stats.energy
+    food = stats.food
+    water = stats.water
+    age = stats.age
+    attack = stats.attack
     
 
 func _ready():
+    
     if stats:
         pass
 
+func _process(_delta):
+    food -= 1
+    if food < 50:
+        print("Eating!")
+        eat()
+    print(food)
+    notify_property_list_changed()
 
 
+func eat():
+    food += 50
 
 
 #Functions:
