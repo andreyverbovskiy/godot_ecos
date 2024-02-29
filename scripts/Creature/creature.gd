f8787a7b0446762ce4be290ed63945f0adc141eb
Chyrion <laine.vili@gmail.com>
2024-02-29 21:00:42 +0200

    Begin creature scripting

--- scripts/Creature/creature.gd
+++ scripts/Creature/creature.gd
@@ -1,3 +1,42 @@
-YIPEEEE
+#create class Creature
+#attributes
+##Attack
+##Energy
+##
 
+#Functions:
+#Shitting :) :)
+
+#Dying
+
+#Eating
+
+#Creating (Birthing) creature
+
+#Moving
+
+#Sleeping
+
+class_name Creature
+
+# Attributes
+
+var energy = 100
+var food = 100
+var water = 100
+var attack = 0
+var age = 0
+
+# Initialize with random attribute values
+func _init():
+    # Only randomizing age for now, others start at maxed values 
+    age = randi_range(1,100)
+
+    pass
+
+func _ready():
+    pass
+
+func _process():
+    pass
 
