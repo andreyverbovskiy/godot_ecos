d3290500da2e0e3fe6eeac02e39f5ce3d5dc49a7
PatrickJamesDev <patrick.james@aalto.fi>
2024-03-07 22:02:15 +0200

    i broke dumbass.svg :(

--- scripts/2D game world for testing.gd
+++ scripts/2D game world for testing.gd
@@ -1,8 +1,9 @@
 extends Node
 
+#USED FOR TESTING CREATURE SPAWNING, EATING, BREEDING, DYING, PREDATION ETCS MECHANICS. 
 #2d game board, 1000x1000 tiles, 
 #Food items are randomly distributed across the game board and they randomly spawn
-
+#Creatures live, are born, and die on the game board.
 
 
 # Called when the node enters the scene tree for the first time.
