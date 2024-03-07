1d107ffec89e235df290ec1ebd07df421495a664
PatrickJamesDev <patrick.james@aalto.fi>
2024-03-07 20:51:55 +0200

    added some creature related scripts

---
+++ 2D game world for testing.gd
@@ -0,0 +1,15 @@
+extends Node
+
+#2d game board, 1000x1000 tiles, 
+#Food items are randomly distributed across the game board and they randomly spawn
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scripts/Creature/creature.gd
+++ scripts/Creature/creature.gd
@@ -1,3 +1,4 @@
+#Creature attributes must have limits, i.e. 9999 attack 9999 energy is not possible
 #create class Creature
 #attributes
 ##Attack
@@ -6,14 +7,6 @@
 
 #Functions:
 #Shitting :) :)
-
-#Dying
-
-#Eating
-
-#Creating (Birthing) creature
-
-#Moving
 
 #Sleeping
 
@@ -26,17 +19,44 @@ var food = 100
 var water = 100
 var attack = 0
 var age = 0
+var speed = 10
+var position_x = 0
+var position_y = 0
 
 # Initialize with random attribute values
 func _init():
-    # Only randomizing age for now, others start at maxed values 
-    age = randi_range(1,100)
+	# Only randomizing age for now, others start at maxed values 
+	age = randi_range(1,100)
 
-    pass
+	pass
+
+#Creating (Birthing) creature         
+#This takes a certain amount of time, energy, food and water. The stats of the offspring will be the average of the parents + random evolution factor
+func _breeding():
+	
+	
+	#return offspring
+	
+	pass
+
+#Creature consumes nearby food. The food of the creature increases, food nearby gets depleted. 
+func _eating():
+	pass
+	#Creature.food++
+	#World.food--
+	#return
+	
+#Creature dies, game object is removed. A proportional amount of food is released to the location. 
+func _dying():
+	pass
+
+#Creature moves (in simple 2D grid) 
+func _moving():
+	
 
 func _ready():
-    pass
+	pass
 
 func _process():
-    pass
+	pass
 
